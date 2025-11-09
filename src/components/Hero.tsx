f689fe326aa24ba7154e95ac640f759cad7f0691
hhadaroadams <hadaroadams1234@gmail.com>
2025-11-09 10:33:39 +0000

    feat(hero): improve hero section to responsive design

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,23 +5,23 @@ import { Avatar, AvatarImage } from "./ui/avatar"
 
 function Hero() {
   return (
-    <main className="relative bg-[#0D193E] text-white min-h-[95vh] w-full flex justify-center    overflow-hidden">
+    <main className="relative bg-[#0D193E] text-white min-h-[95vh] w-full flex justify-center    overflow-hidden ">
       <div className="absolute -top-10 h-full inset-0 z-0 overflow-hidden ">
         <Image src="/hero-assets/vector-1.svg" alt="Background curve" fill />
       </div>
       <div className="absolute top-30 h-full inset-1 z-0 overflow-hidden ">
         <Image src="/hero-assets/vector-2.svg" alt="Background curve" fill />
       </div>
-      <div className="section-padding w-full  flex flex-col-reverse md:flex-row items-center justify-between gap-10 px-6 sm:px-10 md:px-16 lg:px-24 z-10 ">
+      <div className="section-padding w-full  flex  md:flex-row items-center md:justify-between justify-center  gap-10 px-6 sm:px-10 md:px-16 lg:px-24 z-10 ">
         {/* --- Text Section --- */}
-        <div className="space-y-6 text-center md:text-left max-w-[450px] w-full  ">
-          <div className="relative w-full space-y-3 ">
-            <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight text-nowrap">
+        <div className="space-y-6 text-center md:text-left max-w-[500px] w-full md:mt-44  ">
+          <div className="relative w-full space-y-3">
+            <h1 className="text-2xl sm:text-4xl lg:text-5xl font-bold leading-tight text-nowrap">
               Ideas to <span className="text-[#C3D060]">reality</span>,
               tech-driven
             </h1>
 
-            <p className="text-gray-300 text-sm sm:text-base md:text-lg mx-auto md:mx-0 leading-relaxed">
+            <p className="text-gray-300 text-sm md:text-base lg:text-lg mx-auto md:mx-0 leading-relaxed">
               We build mobile apps and web platforms that solve real problems
               for Ghanaians — technology that&apos;s practical, powerful, and
               makes a difference.
@@ -66,8 +66,8 @@ function Hero() {
         </div>
 
         {/* --- Image Section --- */}
-        <div className="flex justify-center md:justify-end w-full md:w-[55%] z-50 lg:w-[80%] pt-12 md:pt-28 relative h-full">
-          <div className=" relative lg:-left-40 w-[85%] sm:w-[75%] md:w-[90%] lg:w-[1000px] scale-x-[-1]">
+        <div className="max-md:hidden flex justify-center md:justify-end w-full md:w-[90%] z-50 lg:w-[90%] pt-12 md:pt-28 relative h-full">
+          <div className=" relative -left-40 w-[1000px] sm:w-[75%] md:w-[1200px]   scale-x-[-1]">
             <Image
               src="/hero-assets/hero-image.png"
               alt="Infusi LLC"

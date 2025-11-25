11483cd09679eade19dfa3c60d1760f35f6691e4
YounoussaBen <younoussaabdourhaman@gmail.com>
2025-11-25 11:16:12 +0000

    feat: redesign ui pages

--- src/components/contact/index.ts
+++ src/components/contact/index.ts
@@ -3,7 +3,7 @@
  * Components for contact page and forms
  */
 
-export { default as ContactCTA } from "./ContactCTA"
+export { default as ContactCTA } from "../home/BusinessTransform"
 export { default as ContactForm } from "./ContactForm"
 export { default as ContactInfo } from "./ContactInfo"
 export { FormField } from "./FormField"
---
+++ src/components/home/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Home components
+ * Components for home page sections
+ */
+export { default as BlogSection } from "./BlogSection"
+export { default as BusinessTransform } from "./BusinessTransform"
+export { default as DigitalGrowth } from "./DigitalGrowth"
+export { default as Hero } from "./Hero"
+export { default as WorkWithUsCards } from "./WorkWithUsCards"
